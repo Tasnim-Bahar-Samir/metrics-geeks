9ade46452bb60ca482ca3f173ec5a0b739fb86ed
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2024-05-09 16:01:09 +0600

    specify staticfiles_build at settings.py

--- MetricsGeeks/settings.py
+++ MetricsGeeks/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles_build' ,'assets'),
+    os.path.join(BASE_DIR ,'assets'),
 ]
 
 # Default primary key field type
